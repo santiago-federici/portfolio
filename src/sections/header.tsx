48a44cb2442a14c0073acc561475f55969415036
santiago-federici <santiagofederici88@gmail.com>
2024-11-27 11:02:52 -0300

    feat: make background transparent for big screens

---
+++ src/sections/header.tsx
@@ -0,0 +1,38 @@
+import { motion } from "motion/react";
+
+import DesktopNavbar from "@/components/navbar/desktop-navbar";
+import MobileNavbar from "@/components/navbar/mobile-navbar";
+
+export default function Header() {
+  return (
+    <motion.header
+      variants={{
+        initial: {
+          opacity: 0,
+          y: -77,
+        },
+        animate: {
+          opacity: 1,
+          y: 0,
+          transition: {
+            delay: 1.3,
+            duration: 0.6,
+          },
+        },
+      }}
+      initial={"initial"}
+      animate={"animate"}
+      className="bg-popover md:bg-transparent"
+    >
+      <div className="wrapper flex items-center justify-between py-6">
+        <h1 className="text-xl font-semibold hover:text-primary duration-200">
+          <a href="#">Portfolio</a>
+        </h1>
+
+        <DesktopNavbar />
+
+        <MobileNavbar />
+      </div>
+    </motion.header>
+  );
+}
