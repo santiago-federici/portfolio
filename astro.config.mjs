e3d2114df16aa28731885022c65d22b526a79577
santiago-federici <santiagofederici88@gmail.com>
2024-12-02 20:49:20 -0300

    feat: remove intro circle animation and uninstall react

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,7 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import react from '@astrojs/react';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind()]
 });
