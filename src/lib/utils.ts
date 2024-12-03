23664205e956ef2fe96ea435c764f18aa0e76b5f
santiago-federici <santiagofederici88@gmail.com>
2024-12-03 19:32:44 -0300

    feat: create and add animation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,6 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const baseAnimation =
+  "duration-500 animate-in fade-in-5 slide-in-from-bottom-3";
